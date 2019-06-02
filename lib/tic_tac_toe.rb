c749752d018e297d6df2843b919085a88abfc9f4
Warren Yao <warrenyca905@gmail.com>
2019-06-02 17:30:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,6 +45,16 @@ class TicTacToe
     end
   end
   
+  def turn_count
+    count = 0
+    @board.each do|position|
+      if position != " " && position != nil
+        count += 1
+      end
+    end
+    return count
+  end
+  
   def turn(board)
     puts "Please enter 1-9:"
     user_input = gets.strip
