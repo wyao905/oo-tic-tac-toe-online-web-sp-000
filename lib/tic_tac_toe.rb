a2729bf5908d394f3653fbd59a633a71c45d9500
Warren Yao <warrenyca905@gmail.com>
2019-06-02 16:04:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,7 @@ class TicTacToe
   
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
