675fdd5b995678c9aee0fa95cb3172f872e1d7c5
Warren Yao <warrenyca905@gmail.com>
2019-06-02 17:38:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,9 +82,9 @@ class TicTacToe
       win_index_2 = win_combination[1]
       win_index_3 = win_combination[2]
     
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
+      position_1 = @board[win_index_1]
+      position_2 = @board[win_index_2]
+      position_3 = @board[win_index_3]
     
       if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combination
